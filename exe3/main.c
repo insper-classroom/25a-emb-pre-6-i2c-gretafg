ea761e0294e9a82f4f6a8b3bde2477aca85a399c
gretafg <gretafg@al.insper.edu.br>
2025-04-14 17:42:36 -0300

    exercicios

--- exe3/main.c
+++ exe3/main.c
@@ -23,6 +23,12 @@ void i2c_task(void *p) {
 
     // TODO
     // read id chip BMP280
+    uint8_t reg_adress = 0xD0;      
+    uint8_t buffer[1];
+
+    i2c_write_blocking(i2c_default, 0x76, &reg_adress, 1, true); 
+    i2c_read_blocking(i2c_default, 0x76, buffer, 1, false);
+
     printf("BMP280 ID: 0x%X \n", buffer[0]);
 
     while (1) {
